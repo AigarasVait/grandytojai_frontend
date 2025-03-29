0d915f401bef1fa5917ae84f5a69a66a10b46674
AigarasVait <aigvai@ktu.lt>
2025-03-29 15:01:43 +0200

    Fixed Endpoint constant for deal page

--- src/constants/api.js
+++ src/constants/api.js
@@ -2,5 +2,5 @@ export const API_LOCAL_URL = 'http://localhost:8080'
 export const API_BASE_ENDPOINT = '/api'
 
 export const API_COMPUTER_PART_ENDPOINT = '/computerParts'
-export const API_BEST_DEAL_ENDPOINT = '/computerParts/bestDeals'
+export const API_BEST_DEAL_ENDPOINT = '/computerParts/best-deals'
 
