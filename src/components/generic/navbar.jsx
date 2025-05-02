18781533337e73b37349db04f173af4b34f50f84
root <root@DESKTOP-9R539AF.>
2025-05-02 19:10:54 +0000

    Reworked Home page, navbar, and did some other minor tweaks

--- src/components/generic/navbar.jsx
+++ src/components/generic/navbar.jsx
@@ -1,9 +1,13 @@
 import React, { useState } from "react";
 import Logo from "../../assets/logo.svg?react";
 import "./navbar.css";
+import { SearchBar } from './searchBar';
+
+
+
 import { Link } from "react-router-dom";
 
-function Navbar() {
+function Navbar({ setSearchValue }) {
   const [openDropdown, setOpenDropdown] = useState(null);
   let timeoutId = null;
 
@@ -15,102 +19,100 @@ function Navbar() {
   const hideDropdown = () => {
     timeoutId = setTimeout(() => {
       setOpenDropdown(null);
-    });
+    }, 200); // Delay for transition effect
   };
 
   return (
-    <div className="navbar">
-      <Link to="/">
-        <div className="logo-container">
-          <Logo className="logo" />
+    <div className="navbar-container">
+      <div className="navbar">
+        <Link to="/">
+          <div className="logo-container">
+            <Logo className="logo" />
+          </div>
+        </Link>
+
+        <SearchBar setSearchValue={setSearchValue} />
+
+        <div className="right-buttons">
+          <Link to="/best-deals">
+            <button className="deal-button">
+              Geriausi pasiūlymai
+            </button>
+          </Link>
+
+          <Link to="/favorites">
+            <button className="deal-button favorites">
+              Mėgstamos prekės
+            </button>
+          </Link>
         </div>
-      </Link>
-
-      <div className="separator"></div>
-
-      {/* Kompiuterių komponentai */}
-      <div
-        className="dropdown"
-        onMouseEnter={() => showDropdown(1)}
-        onMouseLeave={hideDropdown}
-      >
-        <button className="dropdown-button">Kompiuterių komponentai</button>
-        {openDropdown === 1 && (
-          <div className="dropdown-content">
-            <Link to="/category/gpu">Vaizdo plokštės (GPU)</Link>
-            <Link to="/category/cpu">Procesoriai (CPU)</Link>
-            <Link to="/category/motherboard">Pagrindinės plokštės ir priedai</Link>
-          </div>
-        )}
       </div>
 
-      {/* Aušinimo sistemos ir maitinimas */}
-      <div
-        className="dropdown"
-        onMouseEnter={() => showDropdown(2)}
-        onMouseLeave={hideDropdown}
-      >
-        <button className="dropdown-button">Aušinimo sistemos ir maitinimas</button>
-        {openDropdown === 2 && (
-          <div className="dropdown-content">
-            <Link to="/category/cooler">Aušintuvai</Link>
-            <Link to="/category/psu">Maitinimo blokai (PSU)</Link>
-          </div>
-        )}
+      <div className="bottom-navbar">
+        {/* Dropdown 1 */}
+        <div
+          className="dropdown"
+          onMouseEnter={() => showDropdown(1)}
+          onMouseLeave={hideDropdown}
+        >
+          <button className="dropdown-button">Kompiuterių komponentai &#9660;</button>
+          {openDropdown === 1 && (
+            <div className="dropdown-content">
+              <Link to="/category/gpu">Vaizdo plokštės (GPU)</Link>
+              <Link to="/category/cpu">Procesoriai (CPU)</Link>
+              <Link to="/category/motherboard">Pagrindinės plokštės ir priedai</Link>
+            </div>
+          )}
+        </div>
+
+        {/* Dropdown 2 */}
+        <div
+          className="dropdown"
+          onMouseEnter={() => showDropdown(2)}
+          onMouseLeave={hideDropdown}
+        >
+          <button className="dropdown-button">Aušinimo sistemos ir maitinimas &#9660;</button>
+          {openDropdown === 2 && (
+            <div className="dropdown-content">
+              <Link to="/category/cooler">Aušintuvai</Link>
+              <Link to="/category/psu">Maitinimo blokai (PSU)</Link>
+            </div>
+          )}
+        </div>
+
+        {/* Dropdown 3 */}
+        <div
+          className="dropdown"
+          onMouseEnter={() => showDropdown(3)}
+          onMouseLeave={hideDropdown}
+        >
+          <button className="dropdown-button">
+            Atmintis ir Laikmenos (RAM ir SSD) &#9660;
+          </button>
+          {openDropdown === 3 && (
+            <div className="dropdown-content">
+              <Link to="/category/ram">Operatyvioji atmintis (RAM) </Link>
+              <Link to="/category/ssd">SDD ir HDD</Link>
+            </div>
+          )}
+        </div>
+
+        {/* Dropdown 4 */}
+        <div
+          className="dropdown"
+          onMouseEnter={() => showDropdown(4)}
+          onMouseLeave={hideDropdown}
+        >
+          <button className="dropdown-button">Korpusai ir priedai &#9660;</button>
+          {openDropdown === 4 && (
+            <div className="dropdown-content lower">
+              <Link to="/category/case">Kompiuterių korpusai</Link>
+              <Link to="/category/o">Priedai</Link>
+            </div>
+          )}
+        </div>
       </div>
-
-      {/* Atmintis ir Laikmenos (RAM ir SSD) */}
-      <div
-        className="dropdown"
-        onMouseEnter={() => showDropdown(3)}
-        onMouseLeave={hideDropdown}
-      >
-        <button className="dropdown-button">
-        Atmintis ir Laikmenos (RAM ir SSD)
-        </button>
-        {openDropdown === 3 && (
-          <div className="dropdown-content">
-            <Link to="/category/ram">Operatyvioji atmintis (RAM)</Link>
-            <Link to="/category/ssd">SDD ir HDD</Link>
-          </div>
-        )}
-      </div>
-
-      {/* Korpusai ir priedai */}
-      <div
-        className="dropdown"
-        onMouseEnter={() => showDropdown(3)}
-        onMouseLeave={hideDropdown}
-      >
-        <button className="dropdown-button">
-        Korpusai ir priedai
-        </button>
-        {openDropdown === 3 && (
-          <div className="dropdown-content">
-            <Link to="/category/case">Kompiuterių korpusai</Link>
-            <Link to="/category/o">priedai</Link>
-          </div>
-        )}
-      </div>
-
-      <div className="separator"></div>
-
-      <Link to="/best-deals">
-        <button className="deal-button">
-          Geriausi pasiūlymai
-        </button>
-      </Link>
-
-      <div className="separator"></div>
-
-      <Link to="/favorites">
-        <button className="deal-button favorites">
-          Mėgstamos prekės
-        </button>
-      </Link>
-      
     </div>
-    
   );
 }
 
--- src/components/generic/searchBar.tsx
+++ src/components/generic/searchBar.tsx
@@ -6,18 +6,28 @@ interface SearchBarProps {
   setSearchValue: React.Dispatch<React.SetStateAction<string>>;
 }
 
-export const SearchBar: React.FC<SearchBarProps> = ({setSearchValue}) => {
-    
-    const debounced = useDebouncedCallback(
-        (value) => {
-            setSearchValue(value);
-        },
-        1000
-      );
+export const SearchBar: React.FC<SearchBarProps> = ({ setSearchValue }) => {
 
-    return (
-        <>
-            <input className='search-bar' defaultValue={''} onChange={e => debounced(e.target.value)} />
-        </>
-    );
-  };
+  const debounced = useDebouncedCallback(
+    (value) => {
+      setSearchValue(value);
+    },
+    1000
+  );
+
+  return (
+    <div className="search-wrapper">
+      <svg className="search-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+      </svg>
+      <input
+        className="search-bar"
+        placeholder="Ieškok geriausios kainos..."
+        defaultValue=""
+        onChange={e => debounced(e.target.value)}
+      />
+    </div>
+
+
+  );
+};
