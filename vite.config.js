d73d714eea7b64b1c01a24f5d8d50c75265a5907
JuozasPetryla <juopet2@ktu.lt>
2025-05-04 12:38:41 +0300

    Deployment configs

--- vite.config.js
+++ vite.config.js
@@ -3,23 +3,22 @@ import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
-export default ({ mode }) => {
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+export default ({mode}) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())}
 
   return defineConfig({
     plugins: [
       react(),
-      svgr()
+      svgr()  
     ],
-    ...(mode === "development" && {
-      server: {
-        proxy: {
-          '/api': {
-            target: process.env.VITE_API_URL,
-            changeOrigin: true,
-          },
-        },
-      }
-    })
+    // server: {
+    //   proxy: {
+    //     '/api': {
+    //       target: process.env.VITE_API_URL,
+    //       changeOrigin: true,
+    //     },
+    //   },
+    // },
   });
-};
+  
+} 
