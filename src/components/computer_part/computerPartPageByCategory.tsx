cec326a75e895b086258d778f586417698a0ae25
JuozasPetryla <juopet2@ktu.lt>
2025-05-06 07:47:02 +0300

    Fix revert

--- src/components/computer_part/computerPartPageByCategory.tsx
+++ src/components/computer_part/computerPartPageByCategory.tsx
@@ -1,84 +1,110 @@
-import React, { useState, useEffect, useRef } from "react";
-import { ComputerPart } from "../../models/ComputerPart";
-import { getComputerPartsByType } from "../../api/computerParts";
-import { ComputerPartCard } from "./computerPartCard";
-import "./computerPartList.css";
-import Pagination from "../generic/Pagination";
-import { useLocation, useParams } from "react-router-dom";
+import React, { useState, useEffect, useRef } from 'react';
+import { ComputerPart } from '../../models/ComputerPart';
+import { getComputerPartsByType } from '../../api/computerParts';
+import { ComputerPartCard } from './computerPartCard';
+import './computerPartList.css';
+import Pagination from '../generic/Pagination';
+import { useParams } from 'react-router-dom';
 import Navbar from '../generic/navbar';
-import { ComputerPartList } from "./computerPartList";
+import { ComputerPartList } from './computerPartList';
+import { getFilter, saveFilter } from '../../models/CookieUtils';
 
 interface ComputerPartListProps {
-  currentPage?: number;
-  pageSize?: number;
+	currentPage?: number;
+	pageSize?: number;
 }
 
 export const ComputerPartPageByCategory: React.FC<ComputerPartListProps> = () => {
-  const [computerParts, setComputerParts] = useState<ComputerPart[]>([]);
-  const [error, setError] = useState<string | null>(null);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [pageSize, setPageSize] = useState(40);
-  const { category } = useParams();
-  const location = useLocation();
+	const [computerParts, setComputerParts] = useState<ComputerPart[]>([]);
+	const [error, setError] = useState<string | null>(null);
+	const [currentPage, setCurrentPage] = useState(1);
+	const [pageSize, setPageSize] = useState(40);
+	const { category } = useParams();
+	const [searchValue, setSearchValue] = useState<string>('');
 
-  const params = new URLSearchParams(location.search);
-  const searchValueFromUrl = params.get('search') || '';  // Default to empty string if not found
-  const [searchValue, setSearchValue] = useState<string>(searchValueFromUrl);
+	const useStickyState = (key, defaultValue) => {
+		const [value, setValue] = useState(() => {
+			const saved = localStorage.getItem(key);
+			return saved !== null ? JSON.parse(saved) : defaultValue;
+		});
 
-  useEffect(() => {
-    setCurrentPage(1);
-  }, [category]);
+		useEffect(() => {
+			localStorage.setItem(key, JSON.stringify(value));
+		}, [key, value]);
 
-  useEffect(() => {
+		return [value, setValue];
+	};
 
-    const fetchComputerParts = async () => {
-      
-      try {
-        const computerPartsResponse = await getComputerPartsByType<ComputerPart>(
-          category!.toUpperCase(),
-          pageSize,
-          currentPage
-        );
-        setComputerParts(computerPartsResponse);
-      } catch (err: any) {
-        setError(err.message);
-      }
-    };
-    setComputerParts([]);
-    fetchComputerParts();
-  }, [category, currentPage, pageSize, searchValue]);
+	const [filter, setFilter] = useStickyState('filter', 'name ASC');
 
-  return (
-    <div className="computer-part-list">
-      {error && <p className="error-message">{error}</p>}
-      <Navbar setSearchValue={setSearchValue} />
-      <Pagination
-        currentPage={currentPage}
-        pageSize={pageSize}
-        searchValue={searchValue}
-        setCurrentPage={setCurrentPage}
-        setPageSize={setPageSize}
-        category={category}
-      />
-      {searchValue && <ComputerPartList currentPage={currentPage} pageSize={pageSize} searchValue={searchValue} />}
+	const fetchComputerParts = async () => {
+		try {
+			const computerPartsResponse = await getComputerPartsByType<ComputerPart>(
+				category!.toUpperCase(),
+				pageSize,
+				currentPage,
+				'',
+				filter
+			);
+			setComputerParts(computerPartsResponse);
+		} catch (err: any) {
+			setError(err.message);
+		}
+	};
 
-      <div className="grid-container">
-        {computerParts.length > 0 && !searchValue ? (
-          computerParts.map((computerPart) => (
-            <ComputerPartCard key={`${computerPart.barcode}${computerPart.storeName}`} computerPart={computerPart} />
-          ))
-        ) : (
-          <p>No parts found for this category.</p>
-        )}
-      </div>
-      <Pagination
-        currentPage={currentPage}
-        pageSize={pageSize}
-        searchValue={searchValue}
-        setCurrentPage={setCurrentPage}
-        setPageSize={setPageSize}
-        category={category}
-      />
-    </div>
-  );
+	const handleFilterChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+		setFilter(event.target.value);
+		window.location.reload();
+	};
+
+	useEffect(() => {
+		setCurrentPage(1);
+	}, [category]);
+
+	useEffect(() => {
+		setComputerParts([]);
+		fetchComputerParts();
+	}, [category, currentPage, pageSize]);
+
+	return (
+		<div className='computer-part-list'>
+			{error && <p className='error-message'>{error}</p>}
+			<Navbar setSearchValue={setSearchValue} />
+			<Pagination
+				currentPage={currentPage}
+				pageSize={pageSize}
+				searchValue={searchValue}
+				setCurrentPage={setCurrentPage}
+				setPageSize={setPageSize}
+				category={category}
+			/>
+			<div className='sort'>
+				<p>Rūšiavimas:</p>
+				<select value={filter} onChange={handleFilterChange}>
+					<option value={'name ASC'}>pagal abc</option>
+					<option value={'price ASC'}>pigiausi viršuje</option>
+					<option value={'price DESC'}>brangiausi viršuje</option>
+				</select>
+			</div>
+			{searchValue && <ComputerPartList currentPage={currentPage} pageSize={pageSize} searchValue={searchValue} />}
+
+			<div className='grid-container'>
+				{computerParts.length > 0 ? (
+					computerParts.map((computerPart) => (
+						<ComputerPartCard key={computerPart.barcode} computerPart={computerPart} />
+					))
+				) : (
+					<p>No parts found for this category.</p>
+				)}
+			</div>
+			<Pagination
+				currentPage={currentPage}
+				pageSize={pageSize}
+				searchValue={searchValue}
+				setCurrentPage={setCurrentPage}
+				setPageSize={setPageSize}
+				category={category}
+			/>
+		</div>
+	);
 };
--- src/components/generic/Pagination.tsx
+++ src/components/generic/Pagination.tsx
@@ -1,138 +1,147 @@
 import React, { useState, useEffect } from 'react';
-import { getComputerPartsCount, getComputerPartsCountByCategory } from "../../api/computerParts";
-import "./Pagination.css";
+import { getComputerPartsCount, getComputerPartsCountByCategory } from '../../api/computerParts';
+import './Pagination.css';
 
 interface PaginationProps {
-  setCurrentPage: React.Dispatch<React.SetStateAction<number>>;
-  setPageSize: React.Dispatch<React.SetStateAction<number>>;
-  currentPage: number;
-  pageSize: number;
-  searchValue?: string;
-  category?: string;
+	setCurrentPage: React.Dispatch<React.SetStateAction<number>>;
+	setPageSize: React.Dispatch<React.SetStateAction<number>>;
+	currentPage: number;
+	pageSize: number;
+	searchValue?: string;
+	category?: string;
 }
 
-const Pagination: React.FC<PaginationProps> = ({ setCurrentPage, setPageSize, currentPage, pageSize, searchValue, category }) => {
-    const [totalCount, setTotalCount] = useState(0);
-  
-    // Fetch total count of parts when the component mounts
-    useEffect(() => {
-      const fetchCount = async () => {
-        let count = 0;
-  
-        // Fetch count based on searchValue or category
-        if (searchValue) {
-          count = await getComputerPartsCount(searchValue); // Fetch based on search query
-        } else if (category) {
-          count = await getComputerPartsCountByCategory(category); // Fetch based on category
-        } else {
-          // Default count fetching logic (if no filters are applied)
-          count = await getComputerPartsCount(""); // Fetch count without any filters
-        }
-  
-        setTotalCount(count);
-      };
-  
-      fetchCount();
-    }, [searchValue, category]);
-  
-  
-    // Calculate total pages based on totalCount and pageSize
-    const totalPages = Math.ceil(totalCount / pageSize);
-  
-    // Handlers for page change and page size change
-    const handlePageChange = (page: number) => {
-      if (page > 0 && page <= totalPages) {
-        setCurrentPage(page);
-      }
-    };
-  
-    const handlePageSizeChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-      const size = parseInt(event.target.value);
-      setPageSize(size);
-      setCurrentPage(1); // Reset to the first page when page size changes
-    };
+const Pagination: React.FC<PaginationProps> = ({
+	setCurrentPage,
+	setPageSize,
+	currentPage,
+	pageSize,
+	searchValue,
+	category,
+}) => {
+	const [totalCount, setTotalCount] = useState(0);
 
-    // Create pagination display logic
-    const pageNumbers: (number | string)[] = [];
-    const pageRange = 2;
-    var start = 0;
-    var end = 0;
+	// Fetch total count of parts when the component mounts
+	useEffect(() => {
+		const fetchCount = async () => {
+			const count = await getComputerPartsCount(searchValue); // Fetch total count from your API
+			setTotalCount(count);
+		};
 
-    // Add first page
-    pageNumbers.push(1);
+		fetchCount();
+	}, [searchValue]);
 
-    // Add "..." if there are skipped pages before the current page
-    if (currentPage - pageRange > 3) {
-      pageNumbers.push("...");
-    }
+	useEffect(() => {
+		const fetchCount = async () => {
+			const count = await getComputerPartsCountByCategory(category); // Fetch total count from your API
+			setTotalCount(count);
+		};
 
-    if (currentPage < 6) {
-        start = 2;
-        end = 7;
-    }
-    else if (currentPage > totalPages - 5) {
-        start = totalPages - 6
-        end = totalPages - 1;
-    }
-    else {
-        start = currentPage - pageRange;
-        end = currentPage + pageRange;
-    }
+		fetchCount();
+	}, [category]);
 
-    // Add pages around the current page
-    for (let i = start; i <= end; i++) {
-      if (i > 1 && i < totalPages) {
-        pageNumbers.push(i);
-      }
-    }
+	// Calculate total pages based on totalCount and pageSize
+	const totalPages = Math.ceil(totalCount / pageSize);
 
-    // Add "..." if there are skipped pages after the current page
-    if (currentPage + pageRange < totalPages - 2) {
-      pageNumbers.push("...");
-    }
+	// Handlers for page change and page size change
+	const handlePageChange = (page: number) => {
+		if (page > 0 && page <= totalPages) {
+			setCurrentPage(page);
+		}
+	};
 
-    // Add last page
-    if (totalPages > 1) {
-      pageNumbers.push(totalPages);
-    }
-  
-    return (
-      <div className="pagination">
-        <p>
-            Puslapyje prekių:
-        </p>
-        <select value={pageSize} onChange={handlePageSizeChange}>
-          <option value={40}>40</option>
-          <option value={80}>80</option>
-          <option value={160}>160</option>
-        </select>
-  
-        <button 
-          onClick={() => handlePageChange(currentPage - 1)} 
-          disabled={currentPage === 1}>
-          Previous
-        </button>
-  
-        {pageNumbers.map((page, index) => (
-          <button
-            key={index}
-            onClick={() => typeof page === 'number' && handlePageChange(page)}
-            className={currentPage === page ? 'active' : ''}>
-            {page}
-          </button>
-        ))}
-  
-        <button 
-          onClick={() => handlePageChange(currentPage + 1)} 
-          disabled={currentPage === totalPages}>
-          Next
-        </button>
-        
-        <p>
-            Rasta prekių: {totalCount}
-        </p>
-      </div>
-    );
-  };
-  
+	const handlePageSizeChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+		const size = parseInt(event.target.value);
+		setPageSize(size);
+		setCurrentPage(1); // Reset to the first page when page size changes
+	};
+
+	// Create pagination display logic
+	const pageNumbers: (number | string)[] = [];
+	const pageRange = 2;
+	var start = 0;
+	var end = 0;
+
+	// Add first page
+	pageNumbers.push(1);
+
+	// Add "..." if there are skipped pages before the current page
+	if (currentPage - pageRange > 1) {
+		pageNumbers.push('...');
+	}
+
+	if (currentPage < 2) {
+		start = 2;
+		end = 3;
+	} else if (currentPage > totalPages - 1) {
+		start = totalPages - 1;
+		end = totalPages - 1;
+	} else {
+		start = currentPage - pageRange;
+		end = currentPage + pageRange;
+	}
+
+	// Add pages around the current page
+	for (let i = start; i <= end; i++) {
+		if (i > 1 && i < totalPages) {
+			pageNumbers.push(i);
+		}
+	}
+
+	// Add "..." if there are skipped pages after the current page
+	if (currentPage + pageRange < totalPages - 2) {
+		pageNumbers.push('...');
+	}
+
+	// Add last page
+	if (totalPages > 1) {
+		pageNumbers.push(totalPages);
+	}
+
+	return (
+		<div>
+			<div className='pagination'>
+				<div>
+					<button onClick={() => handlePageChange(currentPage - 1)} disabled={currentPage === 1} className='arrow'>
+						&lt;
+					</button>
+
+					{pageNumbers.map((page, index) => (
+						<button
+							key={index}
+							onClick={() => typeof page === 'number' && handlePageChange(page)}
+							className={currentPage === page ? 'active' : ''}
+						>
+							{page}
+						</button>
+					))}
+
+					<button
+						onClick={() => handlePageChange(currentPage + 1)}
+						disabled={currentPage === totalPages}
+						className='arrow'
+					>
+						&gt;
+					</button>
+				</div>
+			</div>
+
+			<div className='pagination-info'>
+				<div className='items-per-page'>
+					<p>Puslapyje prekių:</p>
+					<select value={pageSize} onChange={handlePageSizeChange}>
+						<option value={40}>40</option>
+						<option value={80}>80</option>
+						<option value={160}>160</option>
+					</select>
+				</div>
+
+				<div className='total-items'>
+					<p>Rasta prekių: {totalCount}</p>
+				</div>
+			</div>
+		</div>
+	);
+};
+
 export default Pagination;
